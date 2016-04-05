ce3c8963ea77f250a073305ed8a7e8fac4f6de66
Jakub Pavlik <pavlk.jakub@gmail.com>
2016-04-05 21:37:21 +0200

    kubernetes finalization and fixes

--- custom.d/21-k8s.sh
+++ custom.d/21-k8s.sh
@@ -10,12 +10,17 @@ chroot_exec wget https://downloads.hypriot.com/docker-hypriot_1.9.1-1_armhf.deb 
 wget -O $R/usr/local/bin/kubelet http://apt.tcpcloud.eu/tmp/k8s_binaries_arm/v1.1.1/kubelet
 wget -O $R/usr/local/bin/kube-proxy http://apt.tcpcloud.eu/tmp/k8s_binaries_arm/v1.1.1/kube-proxy
 
-chroot_exec chmod +x $R/usr/local/bin/kubelet
-chroot_exec chmod +x $R/usr/local/bin/kube-proxy
+chmod +x $R/usr/local/bin/kubelet
+chmod +x $R/usr/local/bin/kube-proxy
 
 chroot_exec pip install opencontrail-kubelet
-#chroot_exec ln -s /usr/local/bin/opencontrail-kubelet-plugin /usr/libexec/kubernetes/kubelet-plugins/net/exec/opencontrail/opencontrail
+chroot_exec mkdir -p /usr/libexec/kubernetes/kubelet-plugins/net/exec/opencontrail/
+chroot_exec ln -s /usr/local/bin/opencontrail-kubelet-plugin /usr/libexec/kubernetes/kubelet-plugins/net/exec/opencontrail/opencontrail
 chroot_exec ln -s /lib/ld-linux-armhf.so.3 /lib/ld-linux.so.3
+
+chroot_exec mkdir -p /etc/kubernetes/certs
+chroot_exec touch /var/log/kube-proxy.log
+chroot_exec touch /var/log/kubelet.log
 
 # /usr/bin/docker daemon -H fd:// --storage-driver=overlay -D
 #chroot_exec "service docker start"
@@ -36,5 +41,45 @@ DAEMON_ARGS="--logtostderr=true --v=2 --kubeconfig=/etc/kubernetes/proxy.kubecon
 EOF
 
 cat << EOF >> $R/etc/default/vrouter-agent
-DAEMON_ARGS=" contrail-vrouter-agent --config_file=/etc/contrail/contrail-vrouter-agent.conf --DEFAULT.log_file=/var/log/contrail/vrouter.log"
+DAEMON_ARGS=" --config_file=/etc/contrail/contrail-vrouter-agent.conf --DEFAULT.log_file=/var/log/contrail/vrouter.log"
+EOF
+
+cat << EOF >> $R/etc/kubernetes/proxy.kubeconfig
+apiVersion: v1
+kind: Config
+current-context: proxy-to-cluster.local
+preferences: {}
+contexts:
+- context:
+    cluster: cluster.local
+    user: proxy
+  name: proxy-to-cluster.local
+clusters:
+- cluster:
+    certificate-authority: /etc/kubernetes/certs/ca.crt
+  name: cluster.local
+users:
+- name: proxy
+  user:
+    token:
+EOF
+
+cat << EOF >> $R/etc/kubernetes/kubelet.kubeconfig
+apiVersion: v1
+kind: Config
+current-context: kubelet-to-cluster.local
+preferences: {}
+clusters:
+- cluster:
+    certificate-authority: /etc/kubernetes/certs/ca.crt
+  name: cluster.local
+contexts:
+- context:
+    cluster: cluster.local
+    user: kubelet
+  name: kubelet-to-cluster.local
+users:
+- name: kubelet
+  user:
+    token:
 EOF
